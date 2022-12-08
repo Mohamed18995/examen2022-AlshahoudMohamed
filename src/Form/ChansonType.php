f0954448e836199627b6650800d137424dcefe41
Mohamed Alshahoud <ay1665ay@gmail.com>
2022-12-08 21:00:01 +0100

    update

--- src/Form/ChansonType.php
+++ src/Form/ChansonType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Date;
 
 class ChansonType extends AbstractType
 {
@@ -19,7 +20,7 @@ class ChansonType extends AbstractType
             ->add('nomAlbum', TextType::class)
             ->add('paroles', TextType::class)
             ->add('auteur', TextType::class)
-            // ->add('DateSortie', DateType::class)
+            // ->add('DateSortie', Datetype::class)
             ->add('genre', EntityType::class, [
                 'class' => Genre::class,
                 'choice_label' => 'nom',
